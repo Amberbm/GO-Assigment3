075670158d6905a89f8c2e79f1d9db170fe081a9
Amberbm <56920715+Amberbm@users.noreply.github.com>
2021-04-08 17:31:40 +0200

    Made 2 (currently identical) versions

---
+++ NormalVersion/Assets/Scripts/Pacdot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pacdot : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.name == "pacman")
+		{
+			GameManager.score += 10;
+		    GameObject[] pacdots = GameObject.FindGameObjectsWithTag("pacdot");
+            Destroy(gameObject);
+
+            PlayerController.dotCount++;
+            PlayerController.cycleTracker = 0;
+
+
+            if (pacdots.Length == 1)
+		    {
+		        GameObject.FindObjectOfType<GameGUINavigation>().LoadLevel();
+		    }
+		}
+	}
+}
